abdcd3c3d8d2fe7b5055fc61a568b711efc9d53c
Dmitry <Savelyev.ds@yandex.ru>
2025-06-21 00:45:47 +0300

    Правка папки билда

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // reactStrictMode: true,
+  // distDir: 'out',
+  output: 'export',
 };
 
 export default nextConfig;
--- src/app/subjects/page.tsx
+++ src/app/subjects/page.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 export default function Subjects() {
     const [text, setText] = useState('')
     useEffect(() => {
-        fetch("http://savelyevdmitrys-backendguides-9e11.twc1.net/api/subjects")
+        fetch("https://savelyevdmitrys-planer-backend-e91e.twc1.net/api/subjects")
             .then(res => res.text())
             .then(resText => setText(resText))
     })
